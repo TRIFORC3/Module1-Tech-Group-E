1a76663a0b3fc3c7390be36f396df1978fac641c
Roland Miranda <roland.miranda@outlook.com>
2019-05-02 15:47:34 -0400

    This commit if for testing...

---
+++ MirandaRoland/SalesManager.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+
+public class SalesManager {
+    private String name, number, password, email, orgName;
+    private ArrayList<InventoryItem> inventoryItems;
+
+    public SalesManager(){}
+    public SalesManager(String name, String number, String password, String email, String orgName){
+        this.name = name;
+        this.number = number;
+        this.password = password;
+        this.email = email;
+        this.orgName = orgName;
+        this.inventoryItems  = new ArrayList<>();
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
