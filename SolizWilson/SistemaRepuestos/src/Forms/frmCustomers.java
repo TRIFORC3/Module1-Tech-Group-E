6bab523db358bf5efa15fff413f9c03d56e06dde
unknown <WJS@WSOLIZ-DV01.jalasoft.local>
2019-05-23 10:15:24 -0700

    SistemadeRepuestos

---
+++ SolizWilson/SistemaRepuestos/src/Forms/frmCustomers.java
@@ -0,0 +1,18 @@
+package Forms;
+
+import javax.swing.*;
+
+public class frmCustomers extends JInternalFrame {
+
+    public frmCustomers() {
+        setClosable(true);
+        setIconifiable(true);
+        setResizable(true);
+        setTitle("Customers");
+        setLayout(null);
+        setSize(650,550);
+
+
+    }
+
+}
